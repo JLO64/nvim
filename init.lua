fdc31d934a4ecf07aaa9c2f929bb4775bc42c537
Julian Lopez <64julianlopez@gmail.com>
2025-07-15 19:37:00 -0700

    Created a new command called "CopyFunction"

--- init.lua
+++ init.lua
@@ -46,6 +46,75 @@ end, {
   end,
 })
 
+vim.api.nvim_create_user_command("CopyFunction", function()
+  local ts_utils = require("nvim-treesitter.ts_utils")
+  local parsers = require("nvim-treesitter.parsers")
+  
+  if not parsers.has_parser() then
+    vim.print("No TreeSitter parser available for this filetype")
+    return
+  end
+  
+  local node = ts_utils.get_node_at_cursor()
+  if not node then
+    vim.print("No node found at cursor")
+    return
+  end
+  
+  -- Walk up the tree to find a function definition
+  local function_node = node
+  while function_node do
+    local node_type = function_node:type()
+    -- Debug: print the current node type
+    -- vim.print("Checking node type: " .. node_type)
+    
+    if node_type == "function_definition" or node_type == "async_function_definition" then
+      break
+    end
+    function_node = function_node:parent()
+  end
+  
+  if not function_node then
+    vim.print("Not inside a function")
+    return
+  end
+  
+  -- Get the function name - try multiple approaches
+  local function_name = nil
+  
+  -- Method 1: Try to get name field
+  for child in function_node:iter_children() do
+    if child:type() == "identifier" then
+      function_name = vim.treesitter.get_node_text(child, 0)
+      break
+    end
+  end
+  
+  -- Method 2: If that didn't work, try getting the second child (common pattern)
+  if not function_name then
+    local children = {}
+    for child in function_node:iter_children() do
+      table.insert(children, child)
+    end
+    if #children >= 2 and children[2]:type() == "identifier" then
+      function_name = vim.treesitter.get_node_text(children[2], 0)
+    end
+  end
+  
+  if not function_name then
+    vim.print("Could not find function name")
+    return
+  end
+  
+  vim.fn.setreg("+", function_name)
+  vim.print("Function name copied to clipboard: " .. function_name)
+end, {
+  bang = false,
+  nargs = 0,
+  force = true,
+  desc = "Copy current function name to clipboard",
+})
+
 vim.api.nvim_create_user_command("FormatJSON", function()
   vim.cmd([[:%!jq .]])
 end, {})
