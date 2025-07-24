461137885bde4c2ef0a00bd6e5133665c6d4058b
Julian Lopez <64julianlopez@gmail.com>
2025-07-24 16:18:51 -0700

    started work on new claude code status line

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,49 +1,3 @@
-local header_1 = [[
- __   __     ______     ______     __   __   __     __    __    
-/\ "-.\ \   /\  ___\   /\  __ \   /\ \ / /  /\ \   /\ "-./  \   
-\ \ \-.  \  \ \  __\   \ \ \/\ \  \ \ \'/   \ \ \  \ \ \-./\ \  
- \ \_\\"\_\  \ \_____\  \ \_____\  \ \__|    \ \_\  \ \_\ \ \_\ 
-  \/_/ \/_/   \/_____/   \/_____/   \/_/      \/_/   \/_/  \/_/ 
- ]]
-local header_2 = [[
-NNNNNNNN        NNNNNNNN                                                              iiii                          
-N:::::::N       N::::::N                                                             i::::i                         
-N::::::::N      N::::::N                                                              iiii                          
-N:::::::::N     N::::::N                                                                                            
-N::::::::::N    N::::::N    eeeeeeeeeeee       ooooooooooo vvvvvvv           vvvvvvviiiiiii    mmmmmmm    mmmmmmm   
-N:::::::::::N   N::::::N  ee::::::::::::ee   oo:::::::::::oov:::::v         v:::::v i:::::i  mm:::::::m  m:::::::mm 
-N:::::::N::::N  N::::::N e::::::eeeee:::::eeo:::::::::::::::ov:::::v       v:::::v   i::::i m::::::::::mm::::::::::m
-N::::::N N::::N N::::::Ne::::::e     e:::::eo:::::ooooo:::::o v:::::v     v:::::v    i::::i m::::::::::::::::::::::m
-N::::::N  N::::N:::::::Ne:::::::eeeee::::::eo::::o     o::::o  v:::::v   v:::::v     i::::i m:::::mmm::::::mmm:::::m
-N::::::N   N:::::::::::Ne:::::::::::::::::e o::::o     o::::o   v:::::v v:::::v      i::::i m::::m   m::::m   m::::m
-N::::::N    N::::::::::Ne::::::eeeeeeeeeee  o::::o     o::::o    v:::::v:::::v       i::::i m::::m   m::::m   m::::m
-N::::::N     N:::::::::Ne:::::::e           o::::o     o::::o     v:::::::::v        i::::i m::::m   m::::m   m::::m
-N::::::N      N::::::::Ne::::::::e          o:::::ooooo:::::o      v:::::::v        i::::::im::::m   m::::m   m::::m
-N::::::N       N:::::::N e::::::::eeeeeeee  o:::::::::::::::o       v:::::v         i::::::im::::m   m::::m   m::::m
-N::::::N        N::::::N  ee:::::::::::::e   oo:::::::::::oo         v:::v          i::::::im::::m   m::::m   m::::m
-NNNNNNNN         NNNNNNN    eeeeeeeeeeeeee     ooooooooooo            vvv           iiiiiiiimmmmmm   mmmmmm   mmmmmm
-]]
-local header_3 = [[
- /$$   /$$                                /$$              
-| $$$ | $$                               |__/              
-| $$$$| $$  /$$$$$$   /$$$$$$  /$$    /$$ /$$ /$$$$$$/$$$$ 
-| $$ $$ $$ /$$__  $$ /$$__  $$|  $$  /$$/| $$| $$_  $$_  $$
-| $$  $$$$| $$$$$$$$| $$  \ $$ \  $$/$$/ | $$| $$ \ $$ \ $$
-| $$\  $$$| $$_____/| $$  | $$  \  $$$/  | $$| $$ | $$ | $$
-| $$ \  $$|  $$$$$$$|  $$$$$$/   \  $/   | $$| $$ | $$ | $$
-|__/  \__/ \_______/ \______/     \_/    |__/|__/ |__/ |__/
-]]
-local header_4 = [[
-
-
-███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
-████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
-██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
-██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
-██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
-╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
-
-]]
 return {
   {
     "snacks.nvim",
@@ -98,11 +52,19 @@ return {
           {
             pane = 2,
             padding = 1,
-            { icon = " ", title = "Git Repositories", section = "projects", indent = 2, padding = 10, gap = 1 },
+            { icon = " ", title = "Git Repositories", section = "projects", indent = 2, padding = 8, gap = 1 },
             { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, gap = 1, padding = 1 },
             {
               section = "terminal",
               cmd = "~/.config/nvim/dashboard_status.sh",
+              height = 1,
+              indent = 9,
+              padding = 1,
+              ttl = 10,
+            },
+            {
+              section = "terminal",
+              cmd = "~/.config/nvim/ccusage_status.sh",
               height = 1,
               indent = 9,
               ttl = 10,
